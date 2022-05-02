be2203de8e9f219d897f21503815d70b6655f65e
Mvsrodri <mvsrodri@hotmail.com>
2022-05-02 16:21:36 -0300

    Implementando funcionalidade EqualsAndHashCode e ToString da biblioteca Lombok
    Implementando as funcionalidade EqualsAndHashCode e ToString da biblioteca Lombok na classe CartItem.java para remover a implementação manual dos métodos Equals,Hash e ToString e dessa forma gerar uma melhoria da manutenabilidade do código.

--- src/main/java/org/agoncal/application/petstore/domain/CartItem.java
+++ src/main/java/org/agoncal/application/petstore/domain/CartItem.java
@@ -2,13 +2,15 @@ package org.agoncal.application.petstore.domain;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 /**
  * @author Antonio Goncalves
  *         http://www.antoniogoncalves.org
  *         --
  */
-
+@EqualsAndHashCode
+@ToString
 public class CartItem {
 
     // ======================================
@@ -58,38 +60,4 @@ public class CartItem {
         this.quantity = quantity;
     }
 
-    // ======================================
-    // =   Methods hash, equals, toString   =
-    // ======================================
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        CartItem cartItem = (CartItem) o;
-
-        if (!item.equals(cartItem.item)) return false;
-        if (!quantity.equals(cartItem.quantity)) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = item.hashCode();
-        result = 31 * result + quantity.hashCode();
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder();
-        sb.append("CartItem");
-        sb.append("{item='").append(item).append('\'');
-        sb.append(", quantity='").append(quantity).append('\'');
-        sb.append('}');
-        return sb.toString();
-    }
 }
